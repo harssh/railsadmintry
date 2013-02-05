b11a0f24fc34ca40544f5a0f0d99fc46904752fe
Harssh <harssh@headfitted.com>
2013-02-05 14:55:03 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsadmintry::Application.config.secret_token = 'd03f236803da4ae63912eaff28d51414457f151a5e5be40e6ffe3307ddcf202ed1238a98b78c92a1d06a78024a7ade166a8d9d43b9d7cfc9087c3f64a88d5178'
